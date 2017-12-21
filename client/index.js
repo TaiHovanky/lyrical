6820a5d726281e8722c92586e5290ad8e9e60357
Tai Hovanky <thovanky@arrivelogistics.com>
2017-12-21 12:00:36 -0600

    'starting-react-components'

--- client/index.js
+++ client/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client'; //gets data from server and stores it locally. framework-agnostic.
+import { ApolloProvider } from 'react-apollo';
+import SongList from './components/SongList'
+
+const client = new ApolloClient({}); //assumes the graphQL server is available on the /graphql route
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <SongList />
+    </ApolloProvider>
+  ); //ApolloProvider is a React component and client is a prop
 };
 
 ReactDOM.render(
