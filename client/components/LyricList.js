0aeaeb46515ac31ee4cb050aa7f03aa1dba0ba1c
Tai Hovanky <tai.hovanky@gmail.com>
2017-12-29 14:37:35 -0600

    'finished'

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
-    onLike (id) {
+    onLike (id, likes) {
         console.log('id', id);
+        this.props.mutate({
+            variables: {id},
+            optimisticResponse: {
+                _typename: 'Mutation',
+                likeLyric: {
+                    id: id,
+                    _typename:'LyricType',
+                    likes: likes + 1
+                }
+            } //guess the optimistic response from backend
+            //this updates the UI before a response is received
+            //after the optimistic update, if actual data from server is different, UI will reflect actual data
+        });
     }
 
     renderLyrics() {
-        return this.props.lyrics.map(({ id, content }) => {
+        return this.props.lyrics.map(({ id, content, likes }) => {
+            console.log('ids',)
             return (
                 <li key={id} className="collection-item">
                     {content}
-                    <i 
-                        onClick={() => this.onLike(id)}
-                        className="material-icons"
-                    >thumb_up</i>
+                    <div className="vote-box">
+                        <i 
+                            onClick={() => this.onLike(id, likes)}
+                            className="material-icons"
+                        >thumb_up</i>
+                        {likes}
+                    </div>
                 </li>
             );
         });
@@ -28,4 +47,13 @@ class LyricList extends Component {
     }
 }
 
-export default LyricList;
+const mutation = gql`
+    mutation LikeLyric($id: ID) {
+        likeLyric(id: $id) {
+            id
+            likes
+        }
+    }
+`;
+
+export default graphql(mutation)(LyricList);
